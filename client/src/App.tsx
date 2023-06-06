a305d4b26446e7f7374f30909bc6844913d6fb26
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-06-06 21:47:13 +0530

    Added navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,25 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "@/scenes/navbar"
 const App = () => {
-  const theme = useMemo(() => createTheme(themeSettings),[])
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className='app'>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard</div>} />
+              <Route path="/predictions" element={<div>Predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
