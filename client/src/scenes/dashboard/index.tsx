36090af42ae681bba71d75b9f8fc979b2283c514
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-06-07 15:32:59 +0530

    Added backend

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -3,7 +3,8 @@ import Row1 from "./Row1"
 import Row2 from "./Row2"
 import Row3 from "./Row3"
 
-const gridTemplateLargeScreen = `
+
+const gridTemplateLargeScreens = `
   "a b c"
   "a b c"
   "a b c"
@@ -14,56 +15,58 @@ const gridTemplateLargeScreen = `
   "g h i"
   "g h j"
   "g h j"
-`
-
-const gridTemplateSmallScreen = `
-"a"
-"a"
-"a"
-"a"
-"b"
-"b"
-"b"
-"b"
-"c"
-"c"
-"c"
-"d"
-"d"
-"d"
-"e"
-"e"
-"f"
-"f"
-"f"
-"g"
-"g"
-"g"
-"h"
-"h"
-"h"
-"h"
-"i"
-"i"
-"j"
-"j"
-`
+`;
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
 
 const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
 
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
-      sx={
-        isAboveMediumScreens ? {
-          gridTemplateColumns: "repeat(3, minmax(370px ,1fr))",
-          gridTemplateRows: "repeat(10, minmax(70px,1fr))",
-          gridTemplateAreas: gridTemplateLargeScreen,
-        } : {
-          gridAutoColumns: "1fr",
-          gridAutoRows: "80px",
-          gridTemplateAreas: gridTemplateSmallScreen,
-        }}
+    sx={
+      isAboveMediumScreens
+        ? {
+            gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+            gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+            gridTemplateAreas: gridTemplateLargeScreens,
+          }
+        : {
+            gridAutoColumns: "1fr",
+            gridAutoRows: "80px",
+            gridTemplateAreas: gridTemplateSmallScreens,
+          }
+    }
     >
       <Row1 />
       <Row2 />
