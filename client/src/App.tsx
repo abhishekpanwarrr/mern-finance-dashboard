56a1a55dbe4185485bc2334e18b82853d263d1eb
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-06-07 10:19:42 +0530

    Added dashboard frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "@/scenes/navbar"
+import Dashboard from "@/scenes/dashboard"
+
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), [])
 
@@ -14,7 +16,7 @@ const App = () => {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>Dashboard</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/predictions" element={<div>Predictions</div>} />
             </Routes>
           </Box>
---
+++ client/src/scenes/dashboard/Row1.tsx
@@ -0,0 +1,17 @@
+import DashboardBox from '@/components/DashboardBox'
+import { useGetKpisQuery } from '@/state/api'
+
+const Row1 = () => {
+    const {data} = useGetKpisQuery()
+    console.log("🚀 ~ file: Row1.tsx:6 ~ Row1 ~ data:", data)
+    
+    return (
+        <>
+            <DashboardBox gridArea="a"></DashboardBox>
+            <DashboardBox gridArea="b"></DashboardBox>
+            <DashboardBox gridArea="c"></DashboardBox>
+        </>
+    )
+}
+
+export default Row1
